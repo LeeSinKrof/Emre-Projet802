67e95f4d1cd4687289143180f9bf1f9e9249fc11
LeeSinKrof <ceylan.emre@laposte.net>
2024-03-13 12:43:04 +0100

    add azure url

--- service/Service.ts
+++ service/Service.ts
@@ -2,7 +2,7 @@ import axios, {type AxiosInstance} from 'axios';
 
 
 export const restAPI: AxiosInstance = axios.create({
-    baseURL: 'http://127.0.0.1:3000',
+    baseURL: 'https://projet802-rapi.azurewebsites.net',
 });
 
 class Service {
